20820a4d400aa5f3be6be85a9103e7e537b14150
Sole Cold <cool.gegeg@gmail.com>
2022-09-17 17:17:03 +0300

    old kpi schedule fix

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ window.onload = entry;
 let subjects;
 
 function entry() {
+    console.log('[index] Entry has been called');
+    console.log('[index], inputs root', inputsRoot);
+
     removeAllChildren(inputsRoot)
     chrome.storage.sync.get(['subjects', 'isEnabled'], function (result) {
+        console.log('[index] Storage get has been called', result, enableToggle);
+
         enableToggle.checked = result.isEnabled;
 
         if (result.subjects === undefined) {
@@ -16,36 +21,41 @@ function entry() {
         for (let i = 0; i < subjects.length; i++) {
             createInput(subjects[i], (event) => {
                 const value = event.target.value;
-                if (value === "")
+                if (value === "") {
                     subjects.splice(i, 1);
-                else
+                } else {
                     subjects[i] = value;
+                }
                 chrome.storage.sync.set({ subjects: subjects }, entry);
             })
         }
         createInput("", (event) => {
             subjects.push(event.target.value);
             chrome.storage.sync.set({ subjects: subjects }, entry);
-        })
-
+        });
     });
 
     enableToggle.onclick = async () => {
+        console.log('[index] enableToggle onclick')
         await chrome.storage.sync.set({ isEnabled: enableToggle.checked })
     }
 }
 
 function removeAllChildren(parent) {
+    console.log('[index] remove all children', parent);
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
 }
 
 function createInput(inputValue, onChange) {
+    console.log('[index] create new input', inputValue);
+
     let input = document.createElement('input');
     input.value = inputValue;
-    input.className = "inputField";
+    input.className = "input-field";
     input.onchange = onChange;
+    input.placeholder = 'Мой предмет';
     inputsRoot.appendChild(input)
 }
 
--- src/scripts/fixer.js
+++ src/scripts/fixer.js
@@ -1,20 +1,27 @@
 let whiteList = []
 
-chrome.storage.sync.get(['subjects', 'isEnabled'], function (result) {
+chrome.storage.sync.get(['subjects', 'isEnabled'], async function (result) {
+    console.log('[Fixer] Getting info from storage', result);
     if (!result.isEnabled) {
         return;
     }
     if (result.subjects === undefined) {
-        chrome.storage.sync.set({ subjects: [] });
+        console.log('[fixer] subjects are undefined')
+        await chrome.storage.sync.set({ subjects: [] });
         whiteList = [];
     } else {
+        console.log('[fixer] whitelist has been set');
         whiteList = Array.from(result.subjects)
     }
+    const spans = document.querySelectorAll(HOST_TO_ORIGIN_MAP[window.location.host]);
+    console.log('[fixer] found spans', spans);
 
-    document.querySelectorAll("td>span ").forEach(filterElement);
+    spans.forEach(filterElement)
 });
 
 chrome.storage.onChanged.addListener(function (changes, namespace) {
+    console.log('[Fixer] Key has changed in storage', changes);
+
     for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
         if (key === "subjects") {
             window.location.reload();
@@ -26,8 +33,13 @@ chrome.storage.onChanged.addListener(function (changes, namespace) {
 });
 
 function filterElement(elem) {
+    console.log('[Fixer] filter element', elem);
+
     const classes = elem.innerText.split(",");
-    const prepods = Array.from(elem.parentElement.querySelectorAll("td>a.plainLink"));
+    const teachers = Array.from(elem.parentElement.querySelectorAll("td>a.plainLink"));
+
+    console.log('[fixer] Classes', classes);
+    console.log('[fixer] Teachers', teachers);
 
     for (let i = 0; i < classes.length; i++) {
         const className = classes[i].trim();
@@ -35,8 +47,8 @@ function filterElement(elem) {
             continue;
         }
         classes.splice(i, 1);
-        prepods[i].parentElement.removeChild(prepods[i]);
-        prepods.splice(i, 1);
+        teachers[i].parentElement.removeChild(teachers[i]);
+        teachers.splice(i, 1);
         i--;
     }
 
@@ -48,7 +60,14 @@ function filterElement(elem) {
 }
 
 function removeAllChildren(parent) {
+    console.log('[fixer] Remove all children', parent);
+
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
+}
+
+const HOST_TO_ORIGIN_MAP = {
+    'epi.kpi.ua': 'td > span.disLabel',
+
 }
